6f5652b1d9223be8804ddcf3e115c084461a18ae
Honoriusz <honoriusz@morawiec.pl>
2025-10-14 18:10:01 +0200

    2.11 and 2.12.

--- zestaw2/zadania.py
+++ zestaw2/zadania.py
@@ -1,10 +1,36 @@
-#zadanie 2.10
-
+# zadanie 2.10
+# funkcja oblicza liczbę wyrazów w wieloliniowym stringu
 def count_words(line):
     return len(line.split())
+
+
+# zadanie 2.11
+# funkcja wyswietlajaca string word tak,
+# aby jego znaki byly rozdzielone znakiem podkreslenia
+def special_print(word):
+    print('_'.join(word))
+
+
+# zadanie 2.12
+# Zbudować string stworzony z pierwszych znaków wyrazów ze stringu line.
+# Zbudować napis stworzony z ostatnich znaków wyrazów ze stringu line.
+def build_str(line):
+    tmp = line.split()
+    first = ''
+    last = ''
+    for i in range(len(tmp)):
+        first += tmp[i][0]
+        last += tmp[i][-1]
+
+    print(first)
+    print(last)
 
 
 
 
 if __name__ == '__main__':
-    print(count_words('PyCharm mycharm\nyu'))
+    lin = 'Podstawowe zdanie\nz , kilkoma\nlinijkami'
+    print(count_words(lin))
+    special_print("abc")
+    build_str(lin)
+
