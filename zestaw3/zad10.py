e7c44bcb4ef400d1e047047713b6ab6973e4776f
Honoriusz <honoriusz@morawiec.pl>
2025-10-21 13:45:42 +0200

    completed zad10

--- zestaw3/zad10.py
+++ zestaw3/zad10.py
@@ -6,12 +6,22 @@ na liczby arabskie (podać kilka sposobów tworzenia takiego słownika).
 Mile widziany kod tłumaczący całą liczbę [funkcja roman2int()].
 """
 
+# 1 sposob
 symbols = ["I", "V", "X", "L", "C", "D", "M"]
 d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
+# 2 sposob
+values = [1, 5, 10, 50, 100, 500, 1000]
+d2 = dict.fromkeys(symbols)
+for x in range(7): d2[symbols[x]] = values[x]
+
+# 3 sposob
+d3 = dict()
+for x in range(7):
+    d3[symbols[x]] = values[x]
+
 
 def roman2int(roman):
-
     inti = 0
 
     symbols_index = 6
@@ -19,11 +29,17 @@ def roman2int(roman):
         if (n := roman.find(symbols[symbols_index])) == -1:
             symbols_index -= 1
         elif n == 0:
-            inti += d.get(roman[0])
+            inti += d[roman[0]]
             roman = roman[1:]
+        else:
+            # jesli liczba jest poprawna to n = 1
+            inti += d[roman[1]] - d[roman[0]]
+            roman = roman[n + 1:]
 
     print(inti)
 
 
 r = "MMXXV"
-roman2int(r)
+r2 = "MMCDXLVII"
+r3 = "XL"
+roman2int(r2)
